aad4cfe0a736c6e936470d27ccda1a1531f5bb30
Raineer <delaritaraineer81@gmail.com>
2018-02-09 15:38:53 +0800

    s3 bucket

---
+++ aws-controller.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const aws = require('aws-sdk');
+var secrets = require('./secrets');
+
+const s3 = new aws.S3({
+    signatureVersion: 'v4',
+    region: 'eu-central-2'
+});
+
+exports.signedRequest = function( req, res ){
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'private'
+    };
+
+    s3.getSignedUrl("putObject", s3Params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.end();
+      }
+      const returnData = { signedRequest: data, url: `https://${secrets.aws_bucket}.s3.amazonaws.com/${fileName}` };
+
+      return res.json(returnData);
+    });
+}
+
+exports.getFileSignedRequest = function(req, res) {
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: req.params.fileName,
+        Expires: 60
+    };
+
+    s3.getSignedUrl("getObject", s3Params, (err,data) => {
+        return res.json(data);
+    });
+};
+
+exports.listFIles = function(req, res ) {
+    const s3Params = {
+        Bucket: serets.aws_bucket,
+        Delimiter: "/"
+    }; 
+
+    s3.listObjects(s3Params, function(err, data){
+        if(err) {
+            console.log(err);
+            return res.end();
+        }
+        return res.json(data);
+    }); 
+};
+
+exports.deleteFile = function(res, res) {
+    const s3Params = {
+        Bucket: secrets.aws_bucket,
+        Key: req.params.fileName
+    };
+
+   s3.deleteObject(s3Params, function(err, data)  {
+        if(err) {
+            console.log(err);
+            return res.end();
+        }
+        return res.status(200).send({ "msg": "File deleted"});     
+   }); 
+};
---
+++ secrets.js
@@ -0,0 +1,5 @@
+module.exports =  {
+    aws_bucket: process.env.s3_BUCKET,
+    aws_key: process.AWS_ACCESS_KEY_ID,
+    aws_secret: process.env.AWS_SECRET_ACCESS_KEY,
+}
